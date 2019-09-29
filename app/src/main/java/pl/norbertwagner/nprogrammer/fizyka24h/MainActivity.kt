4dcdd6a3e5bc40588708d6da6218b09c314c0f1f
Norbert Wagner <wagner.norbert277@gmail.com>
2019-09-29 12:32:12 +0200

    Item view score

--- app/src/main/java/pl/norbertwagner/nprogrammer/fizyka24h/MainActivity.kt
+++ app/src/main/java/pl/norbertwagner/nprogrammer/fizyka24h/MainActivity.kt
@@ -1,12 +1,23 @@
 package pl.norbertwagner.nprogrammer.fizyka24h
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun theoryBTClick(v: View){
+        startActivity(Intent(applicationContext, TableOfContentActivity::class.java))
+    }
+    fun exBTClick(v: View){
+
+    }
+    fun scoreBTClick(v: View){
+
     }
 }
---
+++ app/src/main/java/pl/norbertwagner/nprogrammer/fizyka24h/TableOfContentActivity.kt
@@ -0,0 +1,12 @@
+package pl.norbertwagner.nprogrammer.fizyka24h
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableOfContentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_of_content)
+    }
+}
