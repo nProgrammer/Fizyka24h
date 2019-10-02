950b27b05b17507f49f8134c658c132bb37962c2
Norbert Wagner <wagner.norbert277@gmail.com>
2019-10-02 21:29:40 +0200

    ForceActivity GUI

--- app/src/main/java/pl/norbertwagner/nprogrammer/fizyka24h/TableOfContentActivity.kt
+++ app/src/main/java/pl/norbertwagner/nprogrammer/fizyka24h/TableOfContentActivity.kt
@@ -1,12 +1,18 @@
 package pl.norbertwagner.nprogrammer.fizyka24h
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TableOfContentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table_of_content)
+    }
+
+    fun gravityForceClick(v: View){
+        startActivity(Intent(applicationContext, ForceActivity::class.java))
     }
 }
